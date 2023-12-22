c847462d29867d87959d00eb50057740b321782c
Caio Bonani <caio_bonani2002@hotmail.com>
2023-12-21 23:30:31 -0300

    modified split func and created the merge files module

--- src/merge_files.rs
+++ src/merge_files.rs
@@ -3,7 +3,7 @@ use std::io::Read;
 
 pub (crate) fn merge_files (files: Vec<String>, output_dir: &str, output_file: &str, file_extension: &str) {
 
-    let mut splited_data: Vec<u8> = vec![];
+    let mut splitted_data: Vec<u8> = vec![];
 
     for file in files {
 
@@ -11,9 +11,9 @@ pub (crate) fn merge_files (files: Vec<String>, output_dir: &str, output_file: &
         let mut file_op = fs::File::open(file).unwrap();
         file_op.read_to_end(&mut buffer).unwrap();
 
-        splited_data.append(&mut buffer);
+        splitted_data.append(&mut buffer);
     }
 
-    fs::write(output_dir.to_owned() + output_file + '.'.to_string().as_str() + file_extension, splited_data).expect("Error creating the file.");
+    fs::write(output_dir.to_owned() + output_file + '.'.to_string().as_str() + file_extension, splitted_data).expect("Error creating the file.");
 
 }
